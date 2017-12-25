bbc5e4819445abd04a06480a8cf6daa42476e094
pnhung177 <pnhung177@acegik.net>
2017-12-25 16:22:24 +0700

    create log utility level methods

--- test/lab/mock_logger.js
+++ test/lab/mock_logger.js
@@ -2,17 +2,20 @@
 
 var debugx = require('debug')('tdd:logolite:MockLogger');
 
-var MockLogger = function() {
+var MockLogger = function(params) {
+	params = params || {};
 	var self = this;
-	var _cachedMessages = [];
-	var _logLevels = ['error', 'warn', 'info', 'debug', 'trace', 'verbose'];
-	var _logPosition = 0;
-
-	var _isEnabledFor = function(level) {
-		false && debugx.enabled && debugx('_isEnabledFor: %s / %s / %s', 
-			level, _logLevels.indexOf(level), _logPosition);
-		return _logLevels.indexOf(level) <= _logPosition;
+	var _logLevelMap = {
+		error: 0,
+		warn: 1,
+		info: 2,
+		debug: 3,
+		trace: 4,
+		verbose: 5
 	}
+	var _logLevels = Object.keys(_logLevelMap);
+	var _logPosition = 0;
+	var _cachedMessages = [];
 
 	this.log = function(level) {
 		if (_isEnabledFor(level)) {
@@ -34,9 +37,20 @@ var MockLogger = function() {
 		return this;
 	}
 
-	_logLevels.forEach(function(level) {
-		Object.defineProperty(self, level, {
-			get: function() { return _isEnabledFor(level) },
+	var _isEnabledFor = function(level) {
+		false && debugx.enabled && debugx('_isEnabledFor: %s / %s / %s', 
+			level, _logLevels.indexOf(level), _logPosition);
+		return _logLevels.indexOf(level) <= _logPosition;
+	}
+
+	_logLevels.forEach(function(logLevel) {
+		Object.defineProperty(self, logLevel, {
+			get: function() {
+				if (_isEnabledFor(logLevel)) {
+					return self.log.bind(self, logLevel);
+				}
+				return null;
+			},
 			set: function(val) {}
 		})
 	})
