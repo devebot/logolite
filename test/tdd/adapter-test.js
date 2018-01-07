969a414448f05482cb745161f19946b0da25861b
pnhung177 <pnhung177@acegik.net>
2018-01-07 12:01:41 +0700

    Rename mock-logger -> logmock

--- test/tdd/adapter-test.js
+++ test/tdd/adapter-test.js
@@ -5,7 +5,7 @@ var expect = require('chai').expect;
 var debugx = require('debug')('tdd:logolite:LogAdapter');
 var LogAdapter = require('../../lib/adapter');
 var LogConfig = require('../../lib/config');
-var MockLogger = require('../../lib/mock-logger');
+var MockLogger = require('../../lib/logmock');
 
 describe('logolite.LogAdapter:', function() {
 	this.timeout(1000 * 60 * 60);
