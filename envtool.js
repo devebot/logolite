c0e87549ce56f3cea0acd6c68d93ed992e328362
pnhung177 <pnhung177@acegik.net>
2018-02-08 10:38:56 +0700

    Support  template and nested template parameter object

--- envtool.js
+++ envtool.js
@@ -9,17 +9,22 @@ var EnvTool = function(kwargs) {
   this.setup = function(vars) {
     vars = vars || {};
     Object.keys(vars).forEach(function(key) {
-      if (_store[key] == undefined) {
-        _store[key] = process.env[key];
+      _store[key] = process.env[key];
+      if (vars[key] == null) {
+        delete process.env[key];
+      } else {
+        process.env[key] = vars[key];
       }
-      process.env[key] = vars[key];
     });
     return this;
   }
 
   this.reset = function() {
     Object.keys(_store).forEach(function(key) {
-      process.env[key] = _store[key] || '';
+      delete process.env[key];
+      if (typeof(_store[key]) === 'string') {
+        process.env[key] = _store[key];
+      }
       delete _store[key];
     });
     return this;
